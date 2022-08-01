03bacb4f4f0afdaa2b6dfd8e59656372b8b29aff
Lebedevanata19021988 <reklama.bapb@yandex.ru>
2022-08-01 21:05:23 +0300

    added new files

--- 01.08.2022 Practice/src/Task5.java
+++ 01.08.2022 Practice/src/Task5.java
@@ -1,8 +1,23 @@
+import java.util.DoubleSummaryStatistics;
+import java.util.Scanner;
+
 public class Task5 {
 
-    
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите строку с клавиатры: ");
+        String str = sc.nextLine();
+
+        String[] arrayWorld = str.split(" ");
+        int count = 0;
+        for (int i = 0; i < arrayWorld.length; i++) {
+            if (arrayWorld[i].length() % 2 == 0) {
+                count++;
+            }
+        }
+        System.out.println("Количество слов в строке, у которых четное количество букв: ");
+        System.out.print(count);
 
     }
 }
