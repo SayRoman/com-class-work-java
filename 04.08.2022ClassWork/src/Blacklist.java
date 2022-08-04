edd4945746f97eb1575560b7ed66e66a2c4bbb71
Lebedevanata19021988 <reklama.bapb@yandex.ru>
2022-08-04 20:04:47 +0300

    added new files

--- 04.08.2022ClassWork/src/Blacklist.java
+++ 04.08.2022ClassWork/src/Blacklist.java
@@ -1,2 +1,41 @@
 public class Blacklist {
+    public static void main(String[] args) {
+        Blacklist obj = new Blacklist();
+        String[][] array = new String[][]{
+                {"Name   ", "LastName", " Result"},
+                {"Ivan   ", "  Ivanov", "     10"},
+                {"Petro  ", "   Popov", "      9"},
+                {"Andry  ", " Fedotov", "     10"},
+                {"Antony ", "   Mamov", "      7"},
+                {"Igor   ", "  Vlasov", "      8"}
+        };
+        obj.showArray(array);
+        System.out.println();
+        obj.showArrayOfResults(array);
+
+
+    }
+
+    public void equalsResult(String maxMark, String [][] array){
+        
+    }
+
+    public void showArray(String[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void showArrayOfResults(String[][] array){
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 2; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
 }
