c3c9a479fe7ee1637414ba803661f5438197a749
Lebedevanata19021988 <reklama.bapb@yandex.ru>
2022-08-08 18:30:54 +0300

    added

--- 04.08.2022ClassWork/src/Blacklist.java
+++ 04.08.2022ClassWork/src/Blacklist.java
@@ -3,30 +3,43 @@ public class Blacklist {
         Blacklist obj = new Blacklist();
         String[][] array = new String[][]{
                 {"Name   ", "LastName", " Result"},
-                {"Ivan   ", "  Ivanov", "     10"},
-                {"Petro  ", "   Popov", "      9"},
-                {"Andry  ", " Fedotov", "     10"},
-                {"Antony ", "   Mamov", "      7"},
-                {"Igor   ", "  Vlasov", "      8"}
+                {"Ivan   ", "  Ivanov", "10"},
+                {"Petro  ", "   Popov", "9"},
+                {"Andry  ", " Fedotov", "10"},
+                {"Antony ", "   Mamov", "7"},
+                {"Igor   ", "  Vlasov", "8"}
         };
         obj.showArray(array);
         System.out.println();
         obj.showArrayOfResults(array);
         String maxMark = "10";
+        String equal = "";
+        //obj.equalsResult(array[][]);
 
-    }
-
-    public void equalsResult(String maxMark, String [][] array){
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (array[i][2].equals(maxMark)){
-                    sout
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 2; j < array[i].length; j++) {
+                if (array[i][j].equals(maxMark)) {
+                    System.out.println("Yes");
+                    //equal = "Yes";
                 }
+                //System.out.println(equal);
 
             }
 
         }
     }
+
+   /* public void equalsResult(String maxMark, String[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][2].equals(maxMark)) {
+                    String equal = "Yes";
+                }
+
+            }
+
+        }
+    }*/
 
     public void showArray(String[][] array) {
         for (int i = 0; i < array.length; i++) {
@@ -37,7 +50,7 @@ public class Blacklist {
         }
     }
 
-    public void showArrayOfResults(String[][] array){
+    public void showArrayOfResults(String[][] array) {
         for (int i = 1; i < array.length; i++) {
             for (int j = 2; j < array[i].length; j++) {
                 System.out.print(array[i][j] + " ");
