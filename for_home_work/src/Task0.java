8c712895c91c6b0e5e20eae48ee4ea4821b06142
Lebedevanata19021988 <reklama.bapb@yandex.ru>
2022-08-11 18:28:22 +0300

    added new files

--- for_home_work/src/Task0.java
+++ for_home_work/src/Task0.java
@@ -1,2 +1,33 @@
 public class Task0 {
+    /* *********
+     *
+     *       *
+     *       *
+     * *******
+     */
+    char[][] arrayPlayPlace = new char[10][10];
+
+    public static void main(String[] args) {
+        Task0 obj = new Task0();
+
+        obj.createPlayPlace();
+        obj.showArray();
+    }
+
+    public void createPlayPlace() {
+        for (int i = 0; i < arrayPlayPlace.length; i++) {
+            for (int j = 0; j < arrayPlayPlace[i].length; j++) {
+                arrayPlayPlace[i][j] = '*';
+            }
+        }
+    }
+
+    public void showArray() {
+        for (int i = 0; i < arrayPlayPlace.length; i++) {
+            for (int j = 0; j < arrayPlayPlace[i].length; j++) {
+                System.out.print(arrayPlayPlace[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
