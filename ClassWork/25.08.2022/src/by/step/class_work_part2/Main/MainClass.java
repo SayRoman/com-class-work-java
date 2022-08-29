9f91202e3b5c20e7343c4a1b4827ba711ad5213e
Lebedevanata19021988 <reklama.bapb@yandex.ru>
2022-08-29 21:02:44 +0300

    added new files

--- ClassWork/25.08.2022/src/by/step/class_work_part2/Main/MainClass.java
+++ ClassWork/25.08.2022/src/by/step/class_work_part2/Main/MainClass.java
@@ -9,20 +9,21 @@ import by.step.class_work_part2.Task6.Car;
 public class MainClass {
     public static void main(String[] args) {
 
-        Book [] books = new Book[2];
+        Book[] books = new Book[2];
         Book book1 = new Book("The Hobbit", "Tolkian", "1990", "Druk", "Fantazy", 120);
         Book book2 = new Book("The Lord of the Ring", "Tolkian", "1996", "Druk", "Fantazy", 1200);
 
-        Car [] cars = new Car[1];
-        Car car1 = new Car("Audi", "GeneralMotors", 1989,1.6);
+        Car[] cars = new Car[1];
+        cars[0] = new Car("Audi", "GeneralMotors", 1989, 1.6);
 
-        Person [] population = new Person[1];
-        Person person1 = new Person("Roman", "Kornyshev", "Valerievich", "8 december 1989",
+        Person[] population = new Person[1];
+        population[0] = new Person("Roman", "Kornyshev", "Valerievich", "8 december 1989",
                 "292111972", "Vitebsk", "Belarus", "Pr. Pobedy", books, cars);
 
-        City [] cities = new City[3];
-        City city1 = new City("Vitebsk", "Vitebskaya oblast'", "Belarus", 1, 233222, 212,population);
+        City[] cities = new City[3];
+        cities[0] = new City("Vitebsk", "Vitebskaya oblast'", "Belarus", 1, 233222, 212, population);
 
-        Country Belarus = new Country("Belarus", "Evrazia", 1, 375, "Minsk", "Vitebsk, Minsk, Mogilev",cities);
+        Country Belarus = new Country("Belarus", "Evrazia", 1, 375, "Minsk", "Vitebsk, Minsk, Mogilev", cities);
+
     }
 }
