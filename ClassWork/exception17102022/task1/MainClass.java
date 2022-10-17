59f46b138e4203d4ac7cb9a2babd2487c3915bf3
Lebedevanata19021988 <reklama.bapb@yandex.ru>
2022-10-17 20:51:48 +0300

    dodelat

---
+++ ClassWork/exception17102022/task1/MainClass.java
@@ -0,0 +1,20 @@
+package exception17102022.task1;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+   public void getCorrectNumber (){
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Please, input number from 0 to 9: ");
+       
+
+   }
+
+}
--- ClassWork/practice13102022/packing/Main.java
+++ ClassWork/practice13102022/packing/Main.java
@@ -8,12 +8,12 @@ public class Main {
 
         Number1 number1 = new Number1();
         Number2 number2 = new Number2();
-        NotNumber notNumber = new NotNumber();
+        //NotNumber notNumber = new NotNumber();
 
         System.out.println(number1.i1 == number2.i2);
         System.out.println(number1.i1.equals(number2.i2));
         System.out.println(number1.i1.compareTo(number2.i2));
-        System.out.println(number2.i2.compareTo(notNumber.i3));
+        //System.out.println(number2.i2.compareTo(notNumber.i3));
     }
 
 
