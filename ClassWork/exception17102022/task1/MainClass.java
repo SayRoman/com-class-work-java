91d47ffa8dec71c21a0b5c33ff2822eb56feaf3c
Lebedevanata19021988 <reklama.bapb@yandex.ru>
2022-10-17 21:12:52 +0300

    dodelat

--- ClassWork/exception17102022/task1/MainClass.java
+++ ClassWork/exception17102022/task1/MainClass.java
@@ -4,17 +4,27 @@ import java.util.Scanner;
 
 public class MainClass {
 
+    Integer number;
+
 
     public static void main(String[] args) {
+
+        MainClass mainClass = new MainClass();
+        mainClass.getCorrectNumber();
 
 
     }
 
-   public void getCorrectNumber (){
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Please, input number from 0 to 9: ");
-       String s = "";
+    public void getCorrectNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please, input number from 0 to 9: ");
+        String s = sc.nextLine();
+        try { number = Integer.parseInt(s);
 
-   }
+        } catch (NumberFormatException e){
+            getCorrectNumber();
+        }
+
+    }
 
 }
